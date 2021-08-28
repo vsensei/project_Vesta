35bf22b5f2e516825163aed39a3096b3c1ce7d11
VinishkoSensei <fatefulreaper@gmail.com>
2021-08-29 01:39:30 +0300

    fixed getFirstLetterCapitalizedString function bug

--- src/utils/strings.utils.js
+++ src/utils/strings.utils.js
@@ -1,3 +1,3 @@
 export const getFirstLetterCapitalizedString = name => {
-  return name[0].toUpperCase() + name.slice(1).toLowerCase();
+  return name ? name[0].toUpperCase() + name.slice(1).toLowerCase() : name;
 };
