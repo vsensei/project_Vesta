c1621dd3559fed2b6406362cadd2b3bac04203cf
VinishkoSensei <fatefulreaper@gmail.com>
2021-08-30 19:04:29 +0300

    renamed messagesDummy to chatsDummy and added messagesDummy

--- src/dummies/messagesDummy.js
+++ src/dummies/messagesDummy.js
@@ -1,41 +1,13 @@
 const messagesDummy = {
   1: {
-    name: 'Test Author #1',
-    avatar:
-      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-    message: 'Test message #1',
-    time: '11:11',
-    unreadNumber: 0,
+    1: {userId: 1, text: '123'},
+    2: {userId: 2, text: '456'},
+    3: {userId: 1, text: '123'},
   },
   2: {
-    name: 'Test Author #2',
-    avatar:
-      'https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=100&q=80',
-    message: 'Test answer #2',
-    time: '22:22',
-    unreadNumber: 1,
-  },
-  3: {
-    name: 'Test Author #3',
-    avatar: 'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
-    message: 'Test answer #3',
-    time: '11:11',
-    unreadNumber: 10,
-  },
-  4: {
-    name: 'Test Author #4',
-    avatar:
-      'https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg',
-    message: 'Test answer #4',
-    time: '22:22',
-    unreadNumber: 22,
-  },
-  5: {
-    name: 'Test Author #5',
-    avatar: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
-    message: 'Test answer #5',
-    time: '10:10',
-    unreadNumber: '101',
+    1: {userId: 3, text: '123'},
+    2: {userId: 2, text: '456'},
+    3: {userId: 2, text: '123'},
   },
 };
 
